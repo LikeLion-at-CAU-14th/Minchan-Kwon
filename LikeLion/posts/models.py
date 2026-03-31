99c17e64d1524db8bd3d84c2cd79033dd231e922
tronve <gmc8496@gmail.com>
2026-03-31 23:10:55 +0900

    feat: feadback 반영 코드 수정

--- LikeLion/posts/models.py
+++ LikeLion/posts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from accounts.models import User
+from django.conf import settings
 
 # Create your models here.
 
@@ -11,8 +12,16 @@ class BaseModel(models.Model): # models.Model을 상속받음
     class Meta:
         abstract = True
 
-class Post(BaseModel): # BaseModel을 상속받음
+#카테고리 모델
+class Category(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=20)
 
+    # 누락되었던 def __str__(self): 메서드 추가하였습니다.
+    def __str__(self):
+        return self.name
+
+class Post(BaseModel): # BaseModel을 상속받음
     CHOICES = (
         ('STORED', '보관'),
         ('PUBLISHED', '발행')
@@ -22,23 +31,22 @@ class Post(BaseModel): # BaseModel을 상속받음
     title = models.CharField(max_length=50)
     content = models.TextField()
     status = models.CharField(max_length=10, choices=CHOICES, default='STORED')
-    writer = models.ForeignKey(User, on_delete=models.CASCADE, related_name='post')
+    writer = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='posts')
+    # 게시글에서 카테고리와의 연결을 위해 Post class에 categories 필드를 ManyToMany로 연결
+    # 다대다관계이기에 하나의 게시글은 여러 개의 카테고리에 속할 수 있고, 게시글이 삭제되어도 카테고리는 삭제되지 않음
+    categories = models.ManyToManyField(Category, related_name='posts')
 
     def __str__(self):
         return self.title
     
 # 댓글 모델    
 class Comment(BaseModel): # 작성 시간과 수정 시간을 저장하기 위해 BaseModel class를 상속
-
     id = models.AutoField(primary_key=True)
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
     # 하나의 게시글에 여러개의 댓글이 달릴 수 있어야 하니 다대일 관계를 만들어주는 models.ForeignKey 사용
     # 게시글이 삭제되면 댓글도 삭제되어야 하니 CASCADE
     content = models.TextField()
 
-#카테고리 모델
-class Category(models.Model): 
-    id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=20)
-    post = models.ManyToManyField(Post, related_name='categories')
-    # 다대다관계이기에 하나의 게시글은 여러 개의 카테고리에 속할 수 있고, 게시글이 삭제되어도 카테고리는 삭제되지 않음
+    # 누락되었던 def __str__(self): 메서드 추가하였습니다.
+    def __str__(self):
+        return f"Comment {self.id} on {self.post.title}"
--- LikeLion/posts/views.py
+++ LikeLion/posts/views.py
@@ -6,14 +6,42 @@ from .models import *
 
 # Create your views here.
 @require_http_methods(["GET"])
-def get_post_detail(reqeuest, id):
-    post = get_object_or_404(Post, pk=id)
+def get_post_detail(request, id):
+    # N + 1 문제를 해결해주는 prefetch_related 키워드를 적용하였습니다.
+    post = get_object_or_404(Post.objects.prefetch_related('comments', 'categories'), pk=id)
+
+    # 게시글에 달린 댓글 리스트와 카테고리 정보를 함께 응답할 수 있는 코드를 추가하였습니다.
+    comments = post.comments.all()
+    comment_list = []
+
+    for comment in comments:
+        comment_list.append({
+            "id": comment.id,
+            "content": comment.content,
+            "created_at": comment.created_at.strftime('%Y-%m-%d %H:%M:%S'),           
+        })
+
+    categories = post.categories.all()
+    category_list = []
+
+    for category in categories:
+        category_list.append({
+            "id": category.id,
+            "name": category.name,
+        })
+
     post_detail_json = {
         "id" : post.id,
         "title" : post.title,
         "content" : post.content,
         "status" : post.status,
-        "writer" : post.writer.username
+        "writer" : post.writer.username,
+        # API에 작성일과 수정일을 추가하였습니다. 
+        "created_at": post.created_at.strftime('%Y-%m-%d %H:%M:%S'),
+        "updated_at": post.updated_at.strftime('%Y-%m-%d %H:%M:%S'),
+        # 게시글에 달린 댓글 리스트와 카테고리 정보 추가하였습니다.
+        "categories": category_list,
+        "comments": comment_list,
     }
     return JsonResponse({
         "status" : 200,
