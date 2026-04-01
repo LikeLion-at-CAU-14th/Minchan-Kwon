183b8136d8c9f1abfd7e7e9a0cc5798bf15c1a39
tronve <gmc8496@gmail.com>
2026-04-01 22:44:39 +0900

    feat: 5주차 과제 제출

--- LikeLion/posts/admin.py
+++ LikeLion/posts/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
-from .models import Post
-from .models import Comment
-from .models import Category
+from .models import Post, Comment, Category
 
 # Register your models here.
-admin.site.register(Post)
+
+# [수정] Post 는 아래에서 @admin.register 로 등록하므로 여기서는 제외합니다.
 admin.site.register(Comment)
 admin.site.register(Category)
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'writer', 'status', 'created_at')  # 관리자 목록에 노출될 필드
+    search_fields = ('title', 'content')    # 검색 기능 추가
